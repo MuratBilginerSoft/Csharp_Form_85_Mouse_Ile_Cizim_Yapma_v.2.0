cb4353240442b355ac18acc7c4880ee38e256688
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:22:49 +0300

    Initial commit

---
+++ Mouse/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Mouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "10",
+            "15",
+            "20",
+            "50",
+            "75",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 18);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Kalem Kalınlığı";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Mouse.Properties.Resources.s;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(14, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 32);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(810, 446);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Çizim Yap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Mouse/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mouse
+{
+    public partial class Form1 : Form
+    {
+        #region tanımlamalar
+
+        bool ciz; // true yada false değerlerini alabilir
+
+        int baslaX, baslaY; // mouse un o anki form üzerinde bulunduğu konumun koordinatlarını tutacaklar.
+
+        int kalınlık=3;
+
+        ColorDialog renkseç = new ColorDialog(); // renk seçimini yapmak için gerekli nesnemiz
+
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // mouse un herhangi bir tuşuna basıldığı anda çalışacak kodlar.
+            // mouseDown un anlamıda bu zaten tıklandığı andaki işlemler.
+
+            ciz = true; // çizim ifadesi değerini true yaptık. 
+
+            baslaX = e.X; /* MouseDown yani farenin bir tuşuna tıklandığı anda 
+                           * mouse un yaptığı işlemler e değişkeninde saklanır.
+                           * bizde buradan bu özelliğinden yararlanarak e.X ile
+                           * x koordinatını almış olduk. ve baslaX değişkenine 
+                           * atadık*/
+            baslaY = e.Y; // yukarıdaki işlemin aynısını bir de y koordinatı için yaptık.
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            /* MouseMove ise mouse basılı iken mouse u taşıma işlemi 
+            * yaparken yapılacak işlemleri ifade eder. */
+
+            Graphics g = this.CreateGraphics();  /* çizimi yapabilmek için graphic 
+                                                  * türünde bir g değişkeni oluşturduk. 
+                                                  * ve çizimi nerde yapacaksa biz 
+                                                  * form üzerinde yapacağımız için 
+                                                  * this.CreateGraphics dedik. */
+
+            /* Burada bir kaç bir şeyi anlatmalıyım. Şimdi graphics nesnesi ile
+             * kullanabileceğimiz pek çok çizim aracı ve çizim şekli var. Ve bu çizim
+             * şekilleri kendi özelliklerine göre bir takım parametreler alırlar.
+             * Mesela biz birazdan kullanacağımız Drawline yani düz çizgi şeklinden 
+             * bahsedecek olursak. Drawline 3 tane parametre alır bir çizim yapacak 
+             * aracı 2 bir başlangıç noktası koordinatlarını ve son olarakta bitiş noktası 
+             * koordinatlarını parametre olarak alır. şimdi birazdan kullanacağımız bu 
+             * parametreleri oluşturacağız*/
+            
+            Pen p = new Pen(renkseç.Color, kalınlık); /* kalem aracını p değişkeninde tanımladık 
+                                            * ve kalemi oluşturmasını sağladık. gördüğünüz 
+                                            * gibi kalem 2 parametre almakta birincisi çizim
+                                            * yaparkenki rengi , 2.side kalınlığını belirtecek olan
+                                            * bir sayı değeri alır. */
+
+            Point point1 = new Point(baslaX, baslaY); /* az önce bahsettiğimiz başlangıç
+                                                       * noktalasını oluşturduk. */
+
+            Point point2 = new Point(e.X, e.Y); /* buradada bitiş noktasını tanımladık */
+
+            if (ciz == true) //ciz ifadesi true ise çalışacak kodlar
+            {
+                g.DrawLine(p, point1, point2); /* az önce bahsettiğim gibi g.DrawLine 
+                                                * ile düz çizgi çizileceğini bildirdik. ve
+                                                * parametrelerimizi girdik.*/
+
+                baslaX = e.X; // mouse un durduğu son noktayıda yeni başlangıc değeri olarak atadık */
+                baslaY = e.Y;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            /* MouseUp ise basılı mouse tuşunun bırakıldığı anı ifade eder */
+
+            ciz = false; /* ciz ifadesi false oldu. yani mouse bırakıldığı anda çizim 
+                          * çizim işlemide bitecek demek oluyor. */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://muratbilginerncfkr.blogspot.com/"); 
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            renkseç.ShowDialog();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kalınlık = int.Parse(comboBox1.SelectedItem.ToString());
+        }
+    }
+}
